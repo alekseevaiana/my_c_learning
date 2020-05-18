8dfec23354258ddc72b7f6ef306a6f169d1ccb80
alekseevayana <alekseevayana@bk.ru>
2020-05-18 16:35:25 -0700

    second submit

--- projects/my_printf.c
+++ projects/my_printf.c
@@ -3,148 +3,185 @@
 #include <stdarg.h>
 #include <stdint.h>
 
-void my_putchar(char c) {
+void my_putchar(char c)
+{
     write(1, &c, 1);
 }
 
-void print_str(char* str) {
-    while(*str) {
-        my_putchar(*str);
-        str++;
+int print_str(char* str)
+{
+    int i;
+
+    i = 0;
+    if (str != NULL)
+    {
+        while (str[i] != 0)
+        {
+            my_putchar(str[i]);
+            i++;
+        }
+        return i;
     }
+    return 0;
 }
 
-void uDecimalToStr(unsigned int nb) {
-	if(nb >= 10)
-	{
-		uDecimalToStr(nb / 10);
-		uDecimalToStr(nb % 10);
-	}
-	else
-		my_putchar(nb + '0');
+int u_decimal_to_str(unsigned int nb)
+{
+    int i;
+
+    i = 0;
+    if (nb >= 10)
+    {
+        i = i + u_decimal_to_str(nb / 10);
+        i = i + u_decimal_to_str(nb % 10);
+    }
+    else
+    {
+        my_putchar(nb + '0');
+        i++;
+    }
+    return i;
 }
 
-void decimalToStr(int nb) {
-    if (nb < 0) {
+int decimal_to_str(int nb)
+{
+    int i;
+
+    i = 0;
+    if (nb < 0)
+    {
         nb = nb * -1;
         my_putchar('-');
+        i++;
     }
-    uDecimalToStr(nb);
+    i = i + u_decimal_to_str(nb);
+    return i;
 }
 
-void toOctalStr(unsigned int nb) {
+int to_octal_str(unsigned int nb)
+{
+    int i;
+
+    i = 0;
     if (nb >= 8)
-	{
-		toOctalStr(nb / 8);
-		toOctalStr(nb % 8);
-	}
+    {
+        i = i + to_octal_str(nb / 8);
+        i = i + to_octal_str(nb % 8);
+    }
     else
-		my_putchar(nb + '0');
+    {
+        my_putchar(nb + '0');
+        i++;
+    }
+    return i;
 }
 
-void toHexadecimal(unsigned long int nb) {
+int to_hexadecimal(unsigned long int nb)
+{
+    int i;
+
+    i = 0;
     if (nb >= 16)
-	{
-		toHexadecimal(nb / 16);
-		toHexadecimal(nb % 16);
-	}
-    else if (nb >=10 && nb < 16) {
-        if (nb == 10) {
+    {
+        i = i + to_hexadecimal(nb / 16);
+        i = i + to_hexadecimal(nb % 16);
+    }
+    else if (nb >= 10 && nb < 16)
+    {
+        i++;
+        if (nb == 10)
+        {
             my_putchar('a');
         }
-        else if (nb == 11) {
+        else if (nb == 11)
+        {
             my_putchar('b');
         }
-        else if (nb == 12) {
+        else if (nb == 12)
+        {
             my_putchar('c');
         }
-        else if (nb == 13) {
+        else if (nb == 13)
+        {
             my_putchar('d');
         }
-        else if (nb == 14) {
+        else if (nb == 14)
+        {
             my_putchar('e');
         }
-        else if (nb == 15) {
+        else if (nb == 15)
+        {
             my_putchar('f');
         }
     }
     else
-		my_putchar(nb + '0');
+    {
+        i++;
+        my_putchar(nb + '0');
+    }
+    return i;
 }
 
-int my_printf(char* fmt, ...) {
+int my_printf(char* fmt, ...)
+{
     va_list ap;
     char* p;
     char* sval;
     int ival;
     unsigned int uival;
     intptr_t ptr;
+    int size;
+    int count;
 
     va_start(ap, fmt);
-    for (p = fmt; *p; p++) {
-        if (*p != '%') {
+    count = 0;
+    size = 0;
+    for (p = fmt; *p; p++)
+    {
+        if (*p != '%')
+        {
             my_putchar(*p);
+            count++;
             continue;
         }
-        switch (*++p) {
-        case 'c':
-            ival = va_arg(ap, int);
-            my_putchar(ival);
-            break;
-        case 's':
-            sval = va_arg(ap, char*);
-            print_str(sval);
-            break;
-        case 'd':
-            ival = va_arg(ap, int);
-            decimalToStr(ival);
-            break;
-        case 'u':
-            uival = va_arg(ap, unsigned int); 
-            uDecimalToStr(uival);
-            break;
-        case 'o':
-            uival = va_arg(ap, unsigned int);
-            toOctalStr(uival);
-            break;
-        case 'x':
-            ptr = va_arg(ap, intptr_t);
-            toHexadecimal(ptr);
-            break;
-        case 'p':
-            ptr = va_arg(ap, intptr_t);
-            print_str("0x");
-            toHexadecimal(ptr);
-            break;
-        default:
-            my_putchar(*p);
-            break;
+        switch (*++p)
+        {
+            case 'c':
+                ival = va_arg(ap, int);
+                my_putchar(ival);
+                size++;
+                break;
+            case 's':
+                sval = va_arg(ap, char*);
+                size = size + print_str(sval);
+                break;
+            case 'd':
+                ival = va_arg(ap, int);
+                size = size + decimal_to_str(ival);
+                break;
+            case 'u':
+                uival = va_arg(ap, unsigned int);
+                size = size + u_decimal_to_str(uival);
+                break;
+            case 'o':
+                uival = va_arg(ap, unsigned int);
+                size = size + to_octal_str(uival);
+                break;
+            case 'x':
+                ptr = va_arg(ap, intptr_t);
+                size = size + to_hexadecimal(ptr);
+                break;
+            case 'p':
+                ptr = va_arg(ap, intptr_t);
+                size = print_str("0x");
+                size = size + to_hexadecimal(ptr);
+                break;
+            default:
+                my_putchar(*p);
+                count++;
+                break;
         }
     }
     va_end(ap);
-    return 0;
-}
-
-int main() {
-    char* lStr = "Hello";
-    char letter = 'a';
-
-    printf("printf c and s %c %s\n", 'a', "hello");
-    my_printf("my_printf c and s %c %s\n", 'a', "hello");
-    
-    printf("printf d %d\n", -351);
-    my_printf("my_printf d %d\n", -351);
-
-    printf("printf u %u\n", -351);
-    my_printf("my_printf u %u\n", -351);
-
-    printf("printf o %o\n", 351);
-    my_printf("my_printf o %o\n", 351);
-
-    printf("printf x %x\n", 351);
-    my_printf("my_printf x %x\n", 351);
-
-    printf("printf %%p %p\n", lStr);
-    my_printf("my_printf %p\n", lStr);
-    return 0;
+    return size + count;
 }
